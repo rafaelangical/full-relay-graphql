b1381c068145011377297bd3a4791959f6f9eb74
Rafael Souza <rafaelangical2@gmail.com>
2019-10-23 14:12:22 -0300

    Import product type

--- packages/server/src/TypeDefinition.ts
+++ packages/server/src/TypeDefinition.ts
@@ -1,14 +1,18 @@
 import Dataloader from 'dataloader';
+import { PubSub } from 'graphql-subscriptions';
 
 import { IUser } from './modules/user/UserModel';
+import { IProduct } from './modules/product/ProductModel';
 
 type Key = string;
 
 export type Dataloaders = {
-  UserLoader: Dataloader<Key, IUser>;
+	UserLoader: Dataloader<Key, IUser>;
+	ProductLoader: Dataloader<Key, IProduct>;
 };
 
 export type GraphQLContext = {
-  user?: IUser;
-  dataloaders: Dataloaders;
+	user?: IUser;
+	dataloaders: Dataloaders;
+	pubsub: PubSub;
 };
