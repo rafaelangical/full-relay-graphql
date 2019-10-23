2423762863344c40fa2ebd0d85670f005eaa67e8
Rafael Souza <rafaelangical2@gmail.com>
2019-10-23 14:13:15 -0300

    Add product

--- packages/server/src/pubSub.ts
+++ packages/server/src/pubSub.ts
@@ -1,9 +1,12 @@
 import { PubSub } from 'graphql-subscriptions';
 
 export const EVENTS = {
-  USER: {
-    ADDED: 'USER_ADDED',
-  },
+	USER: {
+		ADDED: 'USER_ADDED'
+	},
+	PRODUCT: {
+		ADDED: 'PRODUCT_ADDED'
+	}
 };
 
 export default new PubSub();
