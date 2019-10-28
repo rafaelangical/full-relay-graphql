2ef755eea0d6acb1ed408f0447d3ff3113a5526b
Rafael Souza <rafaelangical2@gmail.com>
2019-10-28 15:16:41 -0300

    Add page ProductCreate

---
+++ app/src/pages/ProductCreate/Mutation/ProductRegisterMutation.tsx
@@ -0,0 +1,29 @@
+import { commitMutation, graphql } from 'react-relay';
+import { Environment } from '../../../relay';
+
+import { ProductRegisterInput, ProductRegisterMutationResponse } from '../__generated__/ProductRegisterMutation.graphql';
+
+const mutation = graphql`
+	mutation ProductRegisterMutation($input: ProductRegisterInput!) {
+		ProductRegister(input: $input) {
+			error
+		}
+	}
+`;
+
+function commit(
+	input: ProductRegisterInput,
+	onCompleted: (response: ProductRegisterMutationResponse) => void,
+	onError: (error: Error) => void
+) {
+	return commitMutation(Environment, {
+		mutation,
+		variables: {
+			input
+		},
+		onCompleted,
+		onError
+	});
+}
+
+export default { commit };
--- app/src/pages/ProductCreate/ProductCreate.tsx
+++ app/src/pages/ProductCreate/ProductCreate.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { AsyncStorage, Alert } from 'react-native';
 import { NavigationScreenProp } from 'react-navigation';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+
+import ProductRegisterMutation from './Mutation/ProductRegisterMutation';
+import { ProductRegisterMutationResponse } from './Mutation/__generated__/ProductRegisterMutation.graphql';
 
 const Wrapper = styled.View`
 	flex: 1;
@@ -32,11 +35,51 @@ export interface ProductRegisterProps {
 }
 
 function ProductCreate({ navigation }: ProductRegisterProps) {
+	useEffect(() => {
+		const token = AsyncStorage.getItem('TOKEN', null);
+		if (token === null) {
+			navigation.navigate('Login');
+		}
+	}, []);
 	const [ name, setName ] = useState('');
 	const [ barcode, setBarcode ] = useState('');
 	const [ description, setDescription ] = useState('');
 	const [ price, setPrice ] = useState(0);
 	const [ qtd, setQtd ] = useState('');
+
+	function handleRegister() {
+		console.warn('create user');
+		const input = {
+			name,
+			description,
+			barcode,
+			price,
+			qtd
+		};
+
+		const onCompleted = (response: ProductRegisterMutationResponse) => {
+			console.warn('oncompleted create product');
+			console.log('onCompleted');
+			if (!response.ProductRegister) return;
+
+			const { error } = response.ProductRegister;
+
+			console.warn(error);
+			error && Alert.alert(error);
+
+			navigation.navigate('ProductList');
+			//console.warn(token);
+		};
+
+		const onError = (err) => {
+			Alert.alert(err);
+			console.warn('create product error');
+			console.log('onError' + err);
+		};
+
+		ProductRegisterMutation.commit(input, onCompleted, onError);
+	}
+
 	return (
 		<Wrapper>
 			<Title>Cadastro de Produtos</Title>
@@ -58,10 +101,20 @@ function ProductCreate({ navigation }: ProductRegisterProps) {
 				value={description}
 				onChangeText={(value: any) => setDescription(value)}
 			/>
-			<Input name="qtd" placeholder="Quantidade" value={qtd} onChangeText={(value: any) => setQtd(value)} />
-			<Input name="price" placeholder="Preço" value={price} onChangeText={(value: any) => setPrice(value)} />
+			<Input
+				name="qtd"
+				placeholder="Quantidade"
+				value={qtd}
+				onChangeText={(value: any) => setQtd(Number(value))}
+			/>
+			<Input
+				name="price"
+				placeholder="Preço"
+				value={price}
+				onChangeText={(value: any) => setPrice(Number(value))}
+			/>
 
-			<Button>
+			<Button onPress={() => handleRegister()}>
 				<ButtonTextAdd>Cadastrar</ButtonTextAdd>
 			</Button>
 
