0dde9e66158bd40a0f1903c7994200861b72ede1
Rafael Souza <rafaelangical2@gmail.com>
2020-01-07 09:52:55 -0300

    feat(app): update mutation to receive TaskEdge

--- app/src/pages/TaskCreate/Mutation/TaskRegisterMutation.tsx
+++ app/src/pages/TaskCreate/Mutation/TaskRegisterMutation.tsx
@@ -1,33 +1,37 @@
-import {commitMutation, graphql} from 'react-relay';
-import {Environment} from '../../../relay';
+import { commitMutation, graphql } from 'react-relay';
+import { Environment } from '../../../relay';
 
-import {
-  TaskRegisterInput,
-  TaskRegisterMutationResponse,
-} from '../__generated__/TaskRegisterMutation.graphql';
+import { TaskRegisterInput, TaskRegisterMutationResponse } from '../__generated__/TaskRegisterMutation.graphql';
 
 const mutation = graphql`
-  mutation TaskRegisterMutation($input: TaskRegisterInput!) {
-    TaskRegister(input: $input) {
-      task
-      error
-    }
-  }
+	mutation TaskRegisterMutation($input: TaskRegisterInput!) {
+		TaskRegister(input: $input) {
+			task {
+				node {
+					name
+					description
+					_id
+					id
+				}
+			}
+			error
+		}
+	}
 `;
 
 function commit(
-  input: TaskRegisterInput,
-  onCompleted: (response: TaskRegisterMutationResponse) => void,
-  onError: (error: Error) => void,
+	input: TaskRegisterInput,
+	onCompleted: (response: TaskRegisterMutationResponse) => void,
+	onError: (error: Error) => void
 ) {
-  return commitMutation(Environment, {
-    mutation,
-    variables: {
-      input,
-    },
-    onCompleted,
-    onError,
-  });
+	return commitMutation(Environment, {
+		mutation,
+		variables: {
+			input
+		},
+		onCompleted,
+		onError
+	});
 }
 
-export default {commit};
+export default { commit };
